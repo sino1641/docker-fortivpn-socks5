43e10cc1b04559bac1f5f4493c7cfa57e805b645
Sin <13870295+sino1641@users.noreply.github.com>
2023-10-17 00:23:46 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,9 @@
 echo "http/socks5 proxy server: $(hostname -i):8443"
 if [ -z "$TOKEN" ]; then
   echo "TOTP token is not set."
+  cp /etc/openfortivpn/config.edit /etc/openfortivpn/config
 else 
+  cp /etc/openfortivpn/config.edit /etc/openfortivpn/config
   sed -i '$ s/otp=[0-9]\{6\}/otp='"$(oathtool -s 60 --totp $TOKEN)"'/' /etc/openfortivpn/config
 fi
 
