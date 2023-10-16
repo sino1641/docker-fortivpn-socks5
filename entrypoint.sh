e21af86a72758cd3e752d2fdd67e959b492f37f4
Sin <13870295+sino1641@users.noreply.github.com>
2023-10-17 01:26:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,4 @@ else
   cp /etc/openfortivpn/config.edit /etc/openfortivpn/config
   sed -i '$ s/otp=[0-9]\{6\}/otp='"$(oathtool -s 60 --totp $TOKEN)"'/' /etc/openfortivpn/config
 fi
-cat /etc/openfortivpn/config 
 exec "$@"
