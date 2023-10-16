38af488b5774b0a84d4f0e0b91747bfd5d489d96
Sin <13870295+sino1641@users.noreply.github.com>
2023-10-17 00:19:34 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 /usr/bin/glider -listen :8443 &
 echo "http/socks5 proxy server: $(hostname -i):8443"
-exec "$@"
+if [ -z "$TOKEN" ]; then
+  echo "TOTP token is not set."
+else 
+  sed -i '$ s/otp=[0-9]\{6\}/otp='"$(oathtool -s 60 --totp $TOKEN)"'/' /etc/openfortivpn/config
+fi
+
+exec "openfortivpn"
